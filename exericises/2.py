2dfc98ad247753167fc1e2e460df1f5b2e583141
niyomugaboetiene <niyomugaboetiene53@gmail.com>
2025-10-07 15:46:44 +0200

    initial commit

--- exericises/2.py
+++ exericises/2.py
@@ -2,7 +2,8 @@ fruits = ["Apple", "Banana", "Chery", "Orange"]
 
 fruits.append("Mango")
 fruits.remove("Banana")
-
+index = fruits.index("Orange")
+fruits[index] = "Grape"
 
 for fruit in fruits:
     print(fruit)
