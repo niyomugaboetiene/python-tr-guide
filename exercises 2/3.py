2d5a9be042248fe7f45f54c6e8db1557cc57aeda
niyomugaboetiene <niyomugaboetiene53@gmail.com>
2025-10-14 13:39:20 +0200

    python:  performing exercises 2

--- exercises 2/3.py
+++ exercises 2/3.py
@@ -1,6 +1,3 @@
 multiply = 2
 for num in range(11):
-    if num == 0:
-        continue
-    num *= multiply
-    print(num ,"X", multiply, "=", num)
+    print(num ,"X", multiply, "=", num * multiply)
