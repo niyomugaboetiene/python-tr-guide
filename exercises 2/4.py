acb71c22cb3a7a5db4195bb01d9d22d68edb720e
niyomugaboetiene <niyomugaboetiene53@gmail.com>
2025-10-14 13:55:36 +0200

    python:  performing exercises 2

--- exercises 2/4.py
+++ exercises 2/4.py
@@ -1,6 +1,6 @@
-for num in range(2, 21):
+for num in range(2, 21):   # start from 2, since 0 and 1 aren't prime
     for i in range(2, num):
-        if i % num == 1:
-            continue
-
-    print(f"{num} is prime")
+        if num % i == 0:   # if divisible, not prime
+            break
+    else:                  # loop finished without finding a divisor
+        print(f"{num} is prime")
