d6e0dea94eac6b67497890428e261258114cc612
niyomugaboetiene <niyomugaboetiene53@gmail.com>
2025-10-14 13:50:04 +0200

    python:  performing exercises 2

--- exercises 2/4.py
+++ exercises 2/4.py
@@ -1,6 +1,6 @@
-for num in range(0, 21):
-    for i in range(0, num):
-        if i % num == 0:
-            break
+for num in range(2, 21):
+    for i in range(2, num):
+        if num % i == 0:
+            continue
 
-print(f"{num} is prime")
+    print(f"{num} is prime")
