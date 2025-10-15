941672583f0e6255899e0e4863a646fee78f24fb
niyomugaboetiene <niyomugaboetiene53@gmail.com>
2025-10-15 06:44:56 +0200

    python:  performing exercises 2

--- exercises 2/5.py
+++ exercises 2/5.py
@@ -1,6 +1,13 @@
-import math
+# import math
 
-num = int(input("Enter number: "))
-print(f"Factorial of {num} is {math.factorial(num)}")
+# num = int(input("Enter number: "))
+# print(f"Factorial of {num} is {math.factorial(num)}")
 
 # of use logic not built-in function of python
+num = int(input("Enter number: "))
+factorial = 1
+
+for i in range(1, num + 1):
+    factorial *= i
+
+print(f"The factorial of {num} is {factorial}")
