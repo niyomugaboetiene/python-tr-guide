4cd64ee7ad0ce61adfce23ac2290caf2c0f79823
niyomugaboetiene <niyomugaboetiene53@gmail.com>
2025-10-15 06:55:11 +0200

    python:  performing exercises 2

--- exercises 2/6.py
+++ exercises 2/6.py
@@ -1,4 +1,5 @@
 for row in range(0, 6):
     for col in range(1, row + 1):
-        print(col, end="")
-    print()
+        print(col, end="") # * it avoid to go to new line just like this 123
+    print() # & when loop finishes it go to new line 123
+                                                # ! 45678
