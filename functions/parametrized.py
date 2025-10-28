ebeb13bbf62436217559b5b5a1c005c9d4025d52
niyomugaboetiene <niyomugaboetiene53@gmail.com>
2025-10-28 08:30:26 +0200

    learning about functions

--- functions/parametrized.py
+++ functions/parametrized.py
@@ -1,4 +1,8 @@
+#  function is block of code that perfom specific task
+# non-prametrized function is function with no parameters
 def Greet():
     print("Hello")
 
 Greet()
+
+# parametrized function is function with parameters
