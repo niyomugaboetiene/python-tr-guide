fc66544f537ee32df59df5c2b0f2a7eca7a9ddfb
niyomugaboetiene <niyomugaboetiene53@gmail.com>
2025-11-18 09:41:04 +0200

    python: learn about sorting

--- sorting.py
+++ sorting.py
@@ -1,7 +1,8 @@
 students = {'alice': 80, 'erice': 100}
 
-# ascending
+# * ascending
 ascending = list(sorted(students.items()))
+# ? descending
 descending = list(sorted(students.items(), reverse=True))
 print("ascending dictionary", ascending)
 print("descending dictionary", descending)
