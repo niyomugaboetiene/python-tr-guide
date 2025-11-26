46e282bfb9e26645f32c6ddad9f8534a63600a69
niyomugaboetiene <niyomugaboetiene53@gmail.com>
2025-11-26 13:47:31 +0200

    exericises

---
+++ NIYOMUGABO ETIENE /Collections/8.py
@@ -0,0 +1,11 @@
+from collections import ChainMap
+
+dict1 = {
+    "address": "etiene", "age": 12
+}
+dict2 = {
+    "name": "etiene", "age": 14
+}
+
+combination = ChainMap(dict1, dict2)
+print(combination)
