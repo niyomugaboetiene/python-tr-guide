5f9be6e650ee901428909032207801c4ba60f8d2
niyomugaboetiene <niyomugaboetiene53@gmail.com>
2025-11-26 13:32:42 +0200

    exericises

---
+++ NIYOMUGABO ETIENE /Collections/4.py
@@ -0,0 +1,7 @@
+# Create a dictionary of student data (name, age,sex, adress course, class ) and print keys/values.
+
+Student = {
+    "name": "Etiene", "age": 12, "sex": 17, "address": "Bugesera", "course": "Python programming", "class": "L5SOD"
+}
+
+print("Student Details", Student)
