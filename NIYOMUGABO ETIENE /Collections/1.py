4b01e6ba4443497be7503366d956eef5429c1a47
niyomugaboetiene <niyomugaboetiene53@gmail.com>
2025-11-26 13:12:30 +0200

    exericises

--- NIYOMUGABO ETIENE /Collections/1.py
+++ NIYOMUGABO ETIENE /Collections/1.py
@@ -2,4 +2,7 @@ MyList = [1, 2, 3, 4, 5, 6, 7]
 # print(MyList.append(8))
 print("Remove item", MyList.remove(2))
 newValue = MyList[4] = 4
-print(MyList)
+print("After Update", MyList)
+# pop
+MyList.pop()
+print("After pop", MyList)
