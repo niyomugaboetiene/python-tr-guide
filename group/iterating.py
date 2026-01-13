59e696942e2eb0eeb139fdad798d304e2722b7ea
niyomugaboetiene <niyomugaboetiene53@gmail.com>
2026-01-13 09:39:12 +0200

    learn iterating

---
+++ group/iterating.py
@@ -0,0 +1,13 @@
+my_dict = {
+    "name": "Eric",
+    "age": 12,
+    "city": "Kayonza"
+}
+
+for key, values in my_dict.items():
+    # ? print(f"Key: {key}, Values: {values}")
+    # * print(key, values)
+    print("Key", key, "Values", values)
+
+
+# ${}, `${}`, {}
