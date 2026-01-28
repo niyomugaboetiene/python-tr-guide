16892d3c236881deb16757221245bb2027c300ff
niyomugaboetiene <niyomugaboetiene53@gmail.com>
2026-01-28 11:10:33 +0200

    Performing exercises in ML

---
+++ group/exercises 3/4.py
@@ -0,0 +1,30 @@
+class Person:
+    gender =  "Gender"
+    name = "Etiene"
+
+    def PersonDetails(self):
+        print("Gender", self.gender)
+        print("Name", self.name)
+
+class Employee:
+    salary = 45000
+    company = "MG Company ltd"
+
+    def EmployeeDetails(self):
+        print("Gender", self.salary)
+        print("Name", self.company)
+
+class Student:
+    course = "Python programming"
+    age = 20
+
+    def StudentDetails(self):
+        print("Gender", self.course)
+        print("Name", self.age)
+
+class PersonInfo(Student, Employee):
+      pass
+
+personInfo = PersonInfo()
+personInfo.EmployeeDetails()
+personInfo.StudentDetails()
