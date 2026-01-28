e343c100eb1c3ca5ea8d7ac1b95baa680d3795be
niyomugaboetiene <niyomugaboetiene53@gmail.com>
2026-01-28 09:38:01 +0200

    performing another exercises

--- group/exercises 3/1.py
+++ group/exercises 3/1.py
@@ -1,2 +1,16 @@
-    # discuss about query optimization techniques used in mongodb
+# discuss about query optimization techniques used in mongodb
 # explain aggregation operations used in mongodb
+
+class User:
+    def __init__(self, username, password):
+       self.username = username
+       self.password = password
+
+    def login(self):
+          if self.username == "Etiene" and self.password == "123":
+              print("Login successfully")
+          else:
+           print("Login failed")
+
+user = User("Etiene", "123")
+user.login()
