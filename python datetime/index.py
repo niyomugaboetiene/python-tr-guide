5d57801124d4465ee38f89d80f0528b7977f495b
niyomugaboetiene <niyomugaboetiene53@gmail.com>
2026-02-10 15:48:01 +0200

    learning python date-time standard library

--- python datetime/index.py
+++ python datetime/index.py
@@ -1,7 +1,9 @@
 from datetime import datetime
+from datetime import date
 
 now = datetime.now()
 print(f"Now {now}")
 
-today = datetime.today()
+# get only date
+today = date.today()
 print(f"To day {today}")
