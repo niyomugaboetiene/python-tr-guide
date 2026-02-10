c10659017516bfa2423db71c77f83d8c3232f44f
niyomugaboetiene <niyomugaboetiene53@gmail.com>
2026-02-10 16:50:34 +0200

    Submitting exercises of datetime

---
+++ python datetime/exercise/1/b.py
@@ -0,0 +1,8 @@
+# Create a program that calculates a person’s age from their birth date.
+from datetime import date
+year = int(input("Enter your birth year: (YYYY)"))
+month = int(input("Enter your moth birth: (MM)"))
+day = int(input("Enter your birth day: (DD)"))
+
+birth_date = date(year, month, day)
+today = date.today()
