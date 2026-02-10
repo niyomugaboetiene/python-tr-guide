ffdbf6fc4f09f48e5f85864198c190e1bc226dec
niyomugaboetiene <niyomugaboetiene53@gmail.com>
2026-02-10 15:50:17 +0200

    learning python date-time standard library

--- python datetime/index.py
+++ python datetime/index.py
@@ -8,6 +8,7 @@ print(f"Now {now}")
 today = date.today()
 print(f"To day {today}")
 
+# remember it get current data(date, time) from your computer
 print(today.month)
 print(today.year)
 print(today.day)
