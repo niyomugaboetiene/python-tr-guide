c6a4e1031ea3c7396e9a6d52f8256d4e7de1e54e
niyomugaboetiene <niyomugaboetiene53@gmail.com>
2026-02-12 10:25:23 +0200

    performing exercise about dates

---
+++ python datetime/exercise/1/g.py
@@ -0,0 +1,13 @@
+# Write a program to check if a given date is in the past or future.
+from datetime import datetime, date
+
+year = int(input("Enter year (YYYY): "))
+month = int(input("Enter month (MM): "))
+day = int(input("Enter day (DD): "))
+
+dates = date(year, month, day)
+
+today = date.today()
+
+if dates > today:
+    print("You are in future date")
